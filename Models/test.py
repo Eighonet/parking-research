020703858354f7625c48156d1e8353c236932855
slavajda02 <slavajda@gmail.com>
2023-12-30 19:17:38 +0100

    added inquirer to test.py

--- Models/test.py
+++ Models/test.py
@@ -4,48 +4,73 @@ from baselines.utils.common_utils import seed_everything, get_device
 from torch.utils.data import DataLoader
 import pandas as pd
 import warnings
-import argparse
+import inquirer
 
-parser = argparse.ArgumentParser(description="Test model on a dataset")
-parser.add_argument("-d", "--dataset", type=str, help="Name of a dataset located in the datasets directory")
-parser.add_argument("-m", "--model", type=str, help="Path to a model to test")
-parser.add_argument("-t", "--type", type=str, choices=models ,help="Type of a model to test")
-parser.add_argument("--pretrained", type=bool, help="Was the model pretrained?", default=False)
-parser.add_argument("-s", "--save", type=bool, help="Export prediciton images", default=False)
-parser.add_argument("--timeit", type=bool, help="Time one inference")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description="Test model on a dataset")
+# parser.add_argument("-d", "--dataset", type=str, help="Name of a dataset located in the datasets directory")
+# parser.add_argument("-m", "--model", type=str, help="Path to a model to test")
+# parser.add_argument("-t", "--type", type=str, choices=models ,help="Type of a model to test")
+# parser.add_argument("--pretrained", type=bool, help="Was the model pretrained?", default=False)
+# parser.add_argument("-s", "--save", type=bool, help="Export prediciton images", default=False)
+# parser.add_argument("--timeit", type=bool, help="Time one inference")
+
+questions = [
+    
+    inquirer.Confirm('pretrained', message = "Using a pretrained model?", default=False),
+    inquirer.List('model',
+                  message="What model do you want to train?",
+                  choices = ["faster_rcnn_mobilenet", "faster_rcnn_mobilenetV3_Large", "faster_rcnn_mobilenetV3_Small", "faster_rcnn_resnet", "faster_rcnn_vgg", "retinanet_mobilenet", "retinanet_resnet", "retinanet_vgg", "retinanet_mobilenetV3_Small", "retinanet_mobilenetV3_Large"],
+                  ignore = lambda x: x["pretrained"] == True),
+    
+    inquirer.List('model',
+                  message="What model do you want to train?",
+                  choices = ["faster_rcnn_mobilenetV3_Large", "faster_rcnn_resnet", "retinanet_resnet"],
+                  ignore = lambda x: x["pretrained"] == False),
+    
+    inquirer.Confirm('retrain', message = "Retrain an existing model?", default=False),
+    inquirer.Path('path',
+                 message="Path to a model to test",
+                 path_type=inquirer.Path.DIRECTORY
+                ),
+    
+    inquirer.Text('dataset',
+                  message="What dataset to use for testing?"),
+    inquirer.Confirm('timeit', message = "Show inference time?", default=True),
+    inquirer.Confirm('save', message = "Save inference to images?", default=True),
+]
+answers = inquirer.prompt(questions, raise_keyboard_interrupt=True)
 
 warnings.filterwarnings("ignore")
 device = get_device()
 
-dataset = args.dataset
+dataset = answers["dataset"]
 
 dataset_path = os.path.join("datasets/"+dataset, dataset)
 #Get wanted model from inter models 
-if args.type == 'faster_rcnn_mobilenet':
-    model = get_model(faster_rcnn_mobilenet_params, args.pretrained)
-elif args.type == 'faster_rcnn_mobilenetV3_Large':
-    model = get_model(faster_rcnn_mobilenetV3_Large_params, args.pretrained)
-elif args.type == 'faster_rcnn_mobilenetV3_Small':
-    model = get_model(faster_rcnn_mobilenetV3_Small_params, args.pretrained)
-elif args.type == 'faster_rcnn_resnet':
-    model = get_model(faster_rcnn_resnet_params, args.pretrained)
-elif args.type == 'faster_rcnn_vgg':
-    model = get_model(faster_rcnn_vgg_params, args.pretrained)
-elif args.type == 'retinanet_mobilenet':
-    model = get_model(retinanet_mobilenet_params, args.pretrained)
-elif args.type == 'retinanet_resnet':
-    model = get_model(retinanet_resnet_params, args.pretrained)
-elif args.type == 'retinanet_vgg':
-    model = get_model(retinanet_vgg_params, args.pretrained)
-elif args.type == 'retinanet_mobilenetV3_Large':
-    model = get_model(retinanet_mobilenetV3_Large_params)
-elif args.type == 'retinanet_mobilenetV3_Small':
-    model = get_model(retinanet_mobilenetV3_Small_params)
+if answers["model"] == 'faster_rcnn_mobilenet':
+    model = get_model(faster_rcnn_mobilenet_params, answers["pretrained"])
+elif answers["model"] == 'faster_rcnn_mobilenetV3_Large':
+    model = get_model(faster_rcnn_mobilenetV3_Large_params, answers["pretrained"])
+elif answers["model"] == 'faster_rcnn_mobilenetV3_Small':
+    model = get_model(faster_rcnn_mobilenetV3_Small_params, answers["pretrained"])
+elif answers["model"] == 'faster_rcnn_resnet':
+    model = get_model(faster_rcnn_resnet_params, answers["pretrained"])
+elif answers["model"] == 'faster_rcnn_vgg':
+    model = get_model(faster_rcnn_vgg_params, answers["pretrained"])
+elif answers["model"] == 'retinanet_mobilenet':
+    model = get_model(retinanet_mobilenet_params, answers["pretrained"])
+elif answers["model"] == 'retinanet_resnet':
+    model = get_model(retinanet_resnet_params, answers["pretrained"])
+elif answers["model"] == 'retinanet_vgg':
+    model = get_model(retinanet_vgg_params, answers["pretrained"])
+elif answers["model"] == 'retinanet_mobilenetV3_Large':
+    model = get_model(retinanet_mobilenetV3_Large_params, answers["pretrained"])
+elif answers["model"] == 'retinanet_mobilenetV3_Small':
+    model = get_model(retinanet_mobilenetV3_Small_params, answers["pretrained"])
 else:
     raise Exception('Invalid model type')
 
-load_model(model, device, args.model)
+load_model(model, device, answers["path"])
 model.to(device)
 
 DIR_INPUT = os.path.join(dataset_path, 'splitted_images')
@@ -66,6 +91,6 @@ test_data_loader = DataLoader(
     collate_fn=collate_fn
 )
 #show_from_dataset(2, test_data_loader)
-acc_list = test_model(model, device, test_data_loader, 0.85, save=args.save, timeit=args.timeit)
+acc_list = test_model(model, device, test_data_loader, 0.85, save=answers["saved"], timeit=answers["timeit"])
 avg_acc = sum(acc_list) / len(acc_list)
 print("Average accuracy on test dataset: %0.2f %%" %(avg_acc*100))
